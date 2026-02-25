2dd93fadb5702ae8271b762ccc8da3cc801be3fd
Yashrajlad28 <yashrajlad6@gmail.com>
2026-02-25 16:50:11 +0530

    Added validations on blood bank, blood request and donation appointment

--- Project/app/models/blood_request.rb
+++ Project/app/models/blood_request.rb
@@ -1,3 +1,12 @@
 class BloodRequest < ApplicationRecord
   belongs_to :hospital
+  belongs_to :blood_bank 
+
+  validates :quantity, numericality: {only_integer: true, message: "Quantity must be integer"}
+  validates :status, presence: true, inclusion: { in: %w(pending fulfilled),
+              message: "%{value} is not a valid size" }
+  
+  validates :blood_group, presence: true, inclusion: { in: ['A+', 'A-', 'B+', 'B-', 'AB+', 
+  'AB-', 'O+', 'O-', 'Rh-', 'Rh+'], message "%{value} is not a valid blood group" }
+
 end
--- Project/db/migrate/20260225071526_create_blood_requests.rb
+++ Project/db/migrate/20260225071526_create_blood_requests.rb
@@ -3,8 +3,8 @@ class CreateBloodRequests < ActiveRecord::Migration[8.1]
     create_table :blood_requests do |t|
       t.integer :quantity
       t.string :status
-      t.integer :accepted_bb_id
       t.string :blood_group
+      t.references :blood_bank, null: false, foreign_key: true
 
       t.timestamps
     end
