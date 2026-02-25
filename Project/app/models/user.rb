b0981a4f01c1555d9749694cfd7e4c734180e3ee
Om Kumavat <144036810+omkumavat@users.noreply.github.com>
2026-02-25 16:08:22 +0530

    Remove allow_blank: false from validations

--- Project/app/models/user.rb
+++ Project/app/models/user.rb
@@ -1,32 +1,32 @@
 class User < ApplicationRecord
   has_many :donation_appointments
 
-  validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }, allow_blank: false
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 50 }
 
-  validates :last_name, presence: true, length: { minimum: 2, maximum: 50 }, allow_blank: false
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 50 }
 
   # validates :email, presence: true
   validates :phone_number, presence: true, uniqueness: true, numericality: { 
     only_integer: true
    },
-   length: { is: 10 }, allow_blank: false
+   length: { is: 10 }
 
-  validates :country, presence: true, length: { minimum: 2, maximum: 50 }, allow_blank: false
+  validates :country, presence: true, length: { minimum: 2, maximum: 50 }
 
-  validates :state, presence: true, length: { minimum: 2, maximum: 50 }, allow_blank: false
+  validates :state, presence: true, length: { minimum: 2, maximum: 50 }
 
-  validates :city, presence: true, length: { minimum: 2, maximum: 50 }, allow_blank: false
+  validates :city, presence: true, length: { minimum: 2, maximum: 50 }
 
-  validates :address, presence: true, length: { minimum: 5, maximum: 100 }, allow_blank: false
+  validates :address, presence: true, length: { minimum: 5, maximum: 100 }
   validates :pincode, presence: true, numericality: { 
     only_integer: true
    },
-   length: { is: 6 }, allow_blank: false
+   length: { is: 6 }
 
   validates :blood_group, presence: true, inclusion: { in: ['A+', 'A-', 'B+', 'B-', 'AB+', 
-  'AB-', 'O+', 'O-', 'Rh-', 'Rh+'] }, allow_blank: false
+  'AB-', 'O+', 'O-', 'Rh-', 'Rh+'] }
 
-  validates :date_of_birth, presence: true, allow_blank: false
+  validates :date_of_birth, presence: true
   
   validate :must_be_18_years_old
  
